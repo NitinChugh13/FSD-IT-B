9ab36a44ad4c656a20b747f3f9a65af6e4ecbd15
Nitin Chugh <nitinchugh6.022@gmail.com>
2025-10-30 15:33:46 +0530

    updated commit

---
+++ REACT-NPM-APP/JS/Module.js
@@ -0,0 +1,11 @@
+function add(x,y){
+    return x+y;
+}
+function sub(x,y){
+    return x-y;
+}
+
+// export default add;
+export {add,sub};
+
+
---
+++ REACT-NPM-APP/JS/ModuleTest.js
@@ -0,0 +1,3 @@
+import {add,sub} from './Module.js'
+console.log(add(3,4));
+console.log(sub(3,4));
